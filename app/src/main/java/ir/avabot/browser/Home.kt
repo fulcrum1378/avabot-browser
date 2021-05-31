03474b354109c42768ddeeeb0c682cbead24397c
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-05-31 21:42:57 +0430

    2021.05.31 - 1

---
+++ app/src/main/java/ir/avabot/browser/Home.kt
@@ -0,0 +1,11 @@
+package ir.avabot.browser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+    }
+}
